9906c6217576673c2f92dcda79a5664a88d2eab7
Akhan91 <70641974+Akhan91@users.noreply.github.com>
2021-09-07 14:03:46 +0200

    buttons for men&women done

--- client/src/Pages/Landing.jsx
+++ client/src/Pages/Landing.jsx
@@ -7,6 +7,11 @@ function Landing() {
       <div className="titleContainer">
         <h1 className="title">bopshop.</h1>
       </div>
+
+        <div className="buttonsContainer">
+          <button className="buttonMen">Men</button>
+          <button className="buttonWomen">Women</button>
+        </div>
     </div>
   )
 }
