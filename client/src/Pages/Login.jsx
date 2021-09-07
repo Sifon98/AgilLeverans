bf8e8bca6529a26dcf274bdd42e8bcbf52a3d3b8
dirren17 <71495689+dirren17@users.noreply.github.com>
2021-09-07 16:48:24 +0200

    Register page

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -2,9 +2,25 @@ import React from 'react'
 
 function Login() {
   return (
-    <div>
-        Hello world
-    </div>
+    <dev className="conteinerLogin">
+      <dev><h1>bopshop.</h1></dev>
+      <form>
+        <input
+          type="text"
+          name="username"
+          placeholder="username"
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          required
+        />
+        <button>Login</button>
+        <button>Register</button>
+      </form>
+    </dev>
   )
 }
 
