cb8fce3e9f9c83776b06246135a96304cd6c8504
Akhan91 <70641974+Akhan91@users.noreply.github.com>
2021-09-07 15:10:05 +0200

    Mens image done

--- client/src/Pages/Landing.jsx
+++ client/src/Pages/Landing.jsx
@@ -3,15 +3,24 @@ import React from 'react'
 
 function Landing() {
   return (
-    <div className="container">
+    <div className="wrapper">
       <div className="titleContainer">
         <h1 className="title">bopshop.</h1>
       </div>
+      <div className="imageContainer">
 
-        <div className="buttonsContainer">
-          <button className="buttonMen">Men</button>
-          <button className="buttonWomen">Women</button>
-        </div>
+      <div className="leftSide">
+        <img className="menImage" src="./src/Men.png" alt="Men" />
+        {/* <button className="womensButton">women</button>
+        <button className="mensButton">mens</button> */}
+      </div>
+      <div className="rightSide">
+        <img className="womenImage" src="./src/Women.png" alt="Women" />
+        {/* <button className="womensButton">women</button>
+        <button className="mensButton">mens</button> */}
+      </div>
+      </div>
+
     </div>
   )
 }
