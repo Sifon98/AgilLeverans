95dba3be64c105f3b54d6f4c1ade4f6ab18c3d45
Akhan91 <70641974+Akhan91@users.noreply.github.com>
2021-09-07 15:59:43 +0200

    adjusted title & images & background

--- client/src/Pages/Landing.jsx
+++ client/src/Pages/Landing.jsx
@@ -4,23 +4,19 @@ import React from 'react'
 function Landing() {
   return (
     <div className="wrapper">
-      <div className="titleContainer">
-        <h1 className="title">bopshop.</h1>
-      </div>
       <div className="imageContainer">
-
       <div className="leftSide">
         <img className="menImage" src="./src/Men.png" alt="Men" />
         {/* <button className="womensButton">women</button>
         <button className="mensButton">mens</button> */}
       </div>
       <div className="rightSide">
+        <div className="titleContainer">
+          <h1 className="title">bopshop.</h1>
+        </div>
         <img className="womenImage" src="./src/Women.png" alt="Women" />
-        {/* <button className="womensButton">women</button>
-        <button className="mensButton">mens</button> */}
       </div>
       </div>
-
     </div>
   )
 }
