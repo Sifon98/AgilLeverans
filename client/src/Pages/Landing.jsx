e2463c065e6d9b8bbecb7c91bb01bfeabef36b5e
Akhan91 <70641974+Akhan91@users.noreply.github.com>
2021-09-07 16:38:34 +0200

    Added redirect to 'Home' when clicking on title

--- client/src/Pages/Landing.jsx
+++ client/src/Pages/Landing.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
+import Home from './Home'
+import { useHistory } from "react-router-dom";
 
 
 function Landing() {
+  const history = useHistory()
   return (
     <div className="wrapper">
-      <div className="imageContainer">
-      <div className="leftSide">
-        <img className="menImage" src="./src/Men.png" alt="Men" />
-        {/* <button className="womensButton">women</button>
-        <button className="mensButton">mens</button> */}
-      </div>
-      <div className="rightSide">
         <div className="titleContainer">
-          <h1 className="title">bopshop.</h1>
+          <h1 onClick={() => history.push("/Home")} className="title">bopshop.</h1>
         </div>
-        <img className="womenImage" src="./src/Women.png" alt="Women" />
-      </div>
+
+      <div className="imageContainer">
+        <div className="leftSide">
+          <img className="menImage" src="./src/Men.png" alt="Men" />
+        </div>
+
+        <div className="rightSide">
+          <img className="womenImage" src="./src/Women.png" alt="Women" />
+        </div>
       </div>
     </div>
   )
