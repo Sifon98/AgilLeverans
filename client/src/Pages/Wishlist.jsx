83f79b24516b860b0db4e6af698692433e58df14
Mudassar Khan <70641965+Mkhaan97@users.noreply.github.com>
2021-09-09 19:47:06 +0200

    navbar fixed
    Co-authored-by: Akhan91 <Akhan91@users.noreply.github.com>

--- client/src/Pages/Wishlist.jsx
+++ client/src/Pages/Wishlist.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import logo from "../img/logo.png"
+import { useHistory } from "react-router-dom";
+
 
 function Wishlist() {
   return (
-    <div>
-        Hello world
+    <div className="wishHeader">
+        <i className="fas fa-user-circle"></i>
+         <img src={logo} />
+        <i class="fas fa-shopping-bag"></i>
+
     </div>
   )
 }
