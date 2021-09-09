c658eed2af6b5ede65f67dfd1a7907c482a2b117
dirren17 <71495689+dirren17@users.noreply.github.com>
2021-09-09 11:05:16 +0200

    login and register

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -3,22 +3,25 @@ import React from 'react'
 function Login() {
   return (
     <dev className="conteinerLogin">
-      <dev><h1>bopshop.</h1></dev>
-      <form>
+      <dev className="yellowSide"></dev>
+      <dev className="head"><h1 className="h1name">bopshop.</h1></dev>
+      <form className="formLogin">
         <input
+          className="inputLogin"
           type="text"
           name="username"
           placeholder="username"
           required
         />
         <input
+          className="inputLogin"
           type="password"
           name="password"
           placeholder="password"
           required
         />
-        <button>Login</button>
-        <button>Register</button>
+        <button className="loginButton">login.</button>
+        <button className="registerButton-loginPage">register.</button>
       </form>
     </dev>
   )
--- client/src/Pages/Register.jsx
+++ client/src/Pages/Register.jsx
@@ -7,30 +7,30 @@ function Register() {
     <dev className="conteinerRegister">
       <dev className="yellowSide"></dev>
       <dev className="head"><h1 className="h1name">bopshop.</h1></dev>
-      <form className="form">
+      <form className="formRegister">
         <input
-          className="input"
+          className="inputRegister"
           type="text"
           name="username"
           placeholder="username"
           required
         />
         <input
-          className="input"
+          className="inputRegister"
           type="email"
           name="email"
           placeholder="e-mail adress"
           required
         />
         <input
-          className="input"
+          className="inputRegister"
           type="password"
           name="password"
           placeholder="password"
           required
         />
         <input
-          className="input"
+          className="inputRegister"
           type="password"
           name="repPassword"
           placeholder="repeat password"
