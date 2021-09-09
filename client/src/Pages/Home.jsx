1905511c61d36f3336a754b2a9e78c595ac271d1
Alexander Gustafsson <alegus753@gmail.com>
2021-09-09 16:53:44 +0200

    Added title that shows gender and category

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -94,6 +94,8 @@ function Home() {
             </div>}
         </div>
       </div>
+      {/* Change titel depending on the categorie and gender */}
+      <h1 className="browsing">{ gender == 1 ? "women." : "men." } { category == 0 ? "shirts." : category == 1 ? "pants." : category == 2 ? "shoes." : null }</h1>
       {/* The list of all the products matching the given parameters */}
       <div className="container">
           {
