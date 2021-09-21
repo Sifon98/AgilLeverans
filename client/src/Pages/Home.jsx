6607c45fa76988e79a43672bc624ea9580488a03
Alexander Gustafsson <alegus753@gmail.com>
2021-09-21 14:26:31 +0200

    Finished sidebar

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -113,12 +113,15 @@ function Home() {
           <span></span>
           {/* List when menu is opened */}
           <ul className="menu">
-            <li id={0} onClick={chooseFilter} className="topText gender">men.</li>
-            <li id={1} onClick={chooseFilter} className="gender">women.</li>
-            <li id={99} onClick={chooseCategory} className="gender">all items.</li>
+            <li id={0} onClick={chooseFilter} className="topText bold">men.</li>
+            <li id={1} onClick={chooseFilter} className="bold">women.</li>
+            <div className="divider" />
+            <li id={99} onClick={chooseCategory} className="bold">all items.</li>
             <li id={0} onClick={chooseCategory}>Shirts</li>
             <li id={1} onClick={chooseCategory}>Pants</li>
             <li id={2} onClick={chooseCategory}>Shoes</li>
+            <div className="divider" />
+            <div className="helper">
             {products[0] && products[0].colors.map(Color => (
               <li id={Color.name} onClick={chooseColor} key={Color.name} style={{background: Color.hex}} className="choose-color">
                 <div className="selected-color" style={color === Color.name ? null : {display: "none"}}>
@@ -130,6 +133,7 @@ function Home() {
               <li id={Sizes} key={Sizes} onClick={chooseSize} className={`choose-size ${Sizes === size ? "selected-size" : ""}`}>{Sizes}</li>
               // <li key={size} className={`size-btn ${selectedSize === size ? "selected" : ""}`} onClick={() => setSelectedSize(size)}>{size}</li>
             ))}
+            </div>
             <li onClick={removeFilter} className="bold">clear filters.</li>
           </ul>
         </div>
