3adf3355f7dc2ff568192052dffea779b8accd1b
Mudassar Khan <70641965+Mkhaan97@users.noreply.github.com>
2021-09-22 14:46:10 +0200

    new-password-button
    Co-authored-by: Akhan91 <Akhan91@users.noreply.github.com>

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -7,35 +7,37 @@ function Profile() {
   const history = useHistory();
   
   const { user, setUser } = useContext(UserContext);
-  const { setNav } = useContext( NavContext );
+  const { setNav } = useContext(NavContext);
   
   
   
-    const handleLogout = async () => {
-      const res = await fetch("/api/logout", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json"
-        }
-      })
-      await res;
-      setNav({path: "/login", direction: 0})
-      setUser(null);
-    }
+  const handleLogout = async () => {
+    const res = await fetch("/api/logout", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      }
+    })
+    await res;
+    setNav({ path: "/login", direction: 0 })
+    setUser(null);
+  }
 
 
     
     
     
-  const [ isDisabledName, setIsDisabledName ] = useState( false );
-  const [ isDisabledEmail, setIsDisabledEmail ] = useState( false );
-  const [ isDisabledPassword, setIsDisabledPassword ] = useState( false );
-  const [ userName, setUserName ] = useState( "" );
-  const [ email, setEmail ] = useState( "" );
-  const [ password, setPassword ] = useState( "" );
-  const [ disableSaveButton, setdisableSaveButton ] = useState( true );
+  const [isDisabledName, setIsDisabledName] = useState(false);
+  const [isDisabledEmail, setIsDisabledEmail] = useState(false);
+  const [isDisabledPassword, setIsDisabledPassword] = useState(false);
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [disableSaveButton, setdisableSaveButton] = useState(true);
   const buttonText = (isDisabled) => isDisabled ? 'Save.' : 'Edit.';
+  
+ 
 
   const handleEdit = async () => {
     const res = await fetch("/api/register", {
@@ -44,7 +46,7 @@ function Profile() {
         Accept: "application/json",
         "Content-Type": "application/json",
       },
-    body: JSON.stringify({userName, email, password})
+      body: JSON.stringify({ userName, email, password })
     })
     
     const getUser = await res.json();
@@ -53,78 +55,104 @@ function Profile() {
   
   const editUser = async (e) => {
     e.preventDefault();
-    setIsDisabledName( boolean => !boolean );
+    setIsDisabledName(boolean => !boolean);
     handleEdit();
-    console.log({userName})
+    console.log({ userName })
   }
 
   const editEmail = async (e) => {
     e.preventDefault();
-    setIsDisabledEmail( boolean => !boolean );
+    setIsDisabledEmail(boolean => !boolean);
     handleEdit();
-    console.log({email})
+    console.log({ email })
   }
 
   const editPass = async (e) => {
     e.preventDefault();
-    setIsDisabledPassword( boolean => !boolean );
+    setIsDisabledPassword(boolean => !boolean);
     handleEdit();
-    console.log({password})
+    console.log({ password })
   }
 
+  const newPass =
+  <>
+    <label className="newPassword" htmlFor="password">Enter New Password</label>
+    <input onChange={(e) => setPassword(e.target.value)} className="inputText" type="password" placeholder="********" disabled={!isDisabledPassword} />
+    {isDisabledPassword
+      ? <button onClick={editPass}>{buttonText(isDisabledPassword)}</button>
+      : <button onClick={(e) => {
+        setIsDisabledPassword(boolean => !boolean), e.preventDefault();
+      }}>{buttonText(isDisabledPassword)}</button>
+    }
+  </>;
+
+
   return (
-    <div className= "profilePageWrapper">
-        <header className="profileTitleContainer">
-          <i  onClick={ () => setNav({path: "/home", direction: 0}) } id="backArrow" className="fas fa-arrow-left"></i>
-          <h1 className="profileTitle">bopshop.</h1>
-          <i id="shoppingCart" style={{visibility: "hidden"}} className="fas fa-shopping-bag"></i>
-        </header>
-        <div className="accountContainer">
-          <h1 className="accountTitle">your account.</h1>
-        </div>
+    <div className="profilePageWrapper">
+      <header className="profileTitleContainer">
+        <i onClick={() => setNav({ path: "/home", direction: 0 })} id="backArrow" className="fas fa-arrow-left"></i>
+        <h1 className="profileTitle">bopshop.</h1>
+        <i id="shoppingCart" style={{ visibility: "hidden" }} className="fas fa-shopping-bag"></i>
+      </header>
+      <div className="accountContainer">
+        <h1 className="accountTitle">your account.</h1>
+      </div>
 
-        <div className="profilePicContainer" style={{display: "none"}}>
-            <i id="profilePic" className="far fa-user-circle"></i>
-          <div className="profileCameraBackground">
-            <i id="profileCameraIcon" className="fas fa-camera"></i>
-          </div>
+      <div className="profilePicContainer" style={{ display: "none" }}>
+        <i id="profilePic" className="far fa-user-circle"></i>
+        <div className="profileCameraBackground">
+          <i id="profileCameraIcon" className="fas fa-camera"></i>
         </div>
+      </div>
 
-        <div className="accountInfoContainer">
+      <div className="accountInfoContainer">
 
         <form className="accountForm" action="" >
-            <label className="username" id="label" htmlFor="username" >username</label>
-            <input onChange={(e) => setUserName(e.target.value)} defaultValue={user && user.username} className="inputText" id="input" type="text" disabled={!isDisabledName} />
-            {isDisabledName 
-            ? <button onClick={editUser}>{buttonText(isDisabledName)}</button> 
-            : <button onClick={(e)  =>  {setIsDisabledName( boolean => !boolean ),e.preventDefault();
+          <label className="username" id="label" htmlFor="username" >username</label>
+          <input onChange={(e) => setUserName(e.target.value)} defaultValue={user && user.username} className="inputText" id="input" type="text" disabled={!isDisabledName} />
+          {isDisabledName
+            ? <button onClick={editUser}>{buttonText(isDisabledName)}</button>
+            : <button onClick={(e) => {
+              setIsDisabledName(boolean => !boolean), e.preventDefault();
             }}>{buttonText(isDisabledName)}</button>
-            }
-            <br />
+          }
+          <br />
 
-            <label  className="emailadress" htmlFor="emailadress" >email</label>
-            <input  onChange={(e) => setEmail(e.target.value)} defaultValue={user && user.email} className="inputText" type="text" disabled={!isDisabledEmail}/>
-            {isDisabledEmail
+          <label className="emailadress" htmlFor="emailadress" >email</label>
+          <input onChange={(e) => setEmail(e.target.value)} defaultValue={user && user.email} className="inputText" type="text" disabled={!isDisabledEmail} />
+          {isDisabledEmail
             ? <button onClick={editEmail}>{buttonText(isDisabledEmail)}</button>
-            : <button onClick={(e)  =>  {setIsDisabledEmail( boolean => !boolean ),e.preventDefault();
+            : <button onClick={(e) => {
+              setIsDisabledEmail(boolean => !boolean), e.preventDefault();
             }}>{buttonText(isDisabledEmail)}</button>
           }
            
-            <br />
+          <br />
           
-            <label  className="password" htmlFor="password">enter new password</label>
-            <input  onChange={(e) => setPassword(e.target.value)} defaultValue={user && user.password} className="inputText" type="password" disabled={!isDisabledPassword}/>
-            <button onClick={editPass}>{buttonText(isDisabledPassword)}</button>
+          <label className="password" htmlFor="password">Current Password</label>
+          <input onChange={(e) => setPassword(e.target.value)} defaultValue={user && user.password} className="inputText" type="password" placeholder="********" disabled={!isDisabledPassword} />
+          {isDisabledPassword
+            ? <><button onClick={(e) => {
+              setIsDisabledPassword(boolean => !boolean), e.preventDefault();
+            }}>{buttonText(isDisabledPassword)}</button>
+            {newPass}</>
+              
+            : <button onClick={(e) => {
+              setIsDisabledPassword(boolean => !boolean), e.preventDefault();
+            }}>{buttonText(isDisabledPassword)}</button>
+          }
+    
         </form>
       
-        </div>
+      </div>
 
-        <footer className="signoutContainer">
+      <footer className="signoutContainer">
         <button className="signoutButton" onClick={handleLogout}>sign out.</button>
-        </footer>
+      </footer>
     </div>
   )
 }
+
 
 export default Profile
 
