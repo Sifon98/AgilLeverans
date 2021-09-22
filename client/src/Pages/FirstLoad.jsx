674eae9eca1d2ca651835a11db11b92bf1d6651d
Alexander Gustafsson <alegus753@gmail.com>
2021-09-22 12:01:22 +0200

    home and login fixes

--- client/src/Pages/FirstLoad.jsx
+++ client/src/Pages/FirstLoad.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useContext } from 'react'
 import { UserContext } from "../context/UserContext";
 import { NavContext } from "../context/NavContext";
 import { useHistory } from "react-router-dom";
+import logoLogin from "../img/logo-login.svg"
 
 function FirstLoad() {
   const history = useHistory();
@@ -19,9 +20,9 @@ function FirstLoad() {
 
   return (
 
-    <div className="conteinerFirstLoad page">
+    <div className="containerFirstLoad page">
         <div className="yellowSide"></div>
-        <div className="head"><h1 className="h1name">bopshop.</h1></div>
+        <img src={logoLogin} />
         <div className="welcomeText">welcome to bopshop.</div>
         <div className="randomLine"></div>
         <button className="loginButton" onClick={() => setNav({path: "/login", direction: 1})}>login.</button>
