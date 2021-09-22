a553af9b1097fe67adfa1535bfadd9eab792865e
Alexander Gustafsson <alegus753@gmail.com>
2021-09-22 15:26:21 +0200

    More components of home

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -7,6 +7,9 @@ import { NavContext } from "../context/NavContext";
 import { UserContext } from "../context/UserContext";
 import LoginHome from "../components/HomePage/LoginHome"
 import SideMenu from "../components/HomePage/SideMenu"
+import DesktopHeader from '../components/HomePage/DesktopHeader';
+import MobileHeader from '../components/HomePage/MobileHeader';
+import SortingButtons from '../components/HomePage/SortingButtons';
 
 
 function Home() {
@@ -153,82 +156,16 @@ function Home() {
   return (
     <div className="page">
       <div className="header">
-        <SideMenu chooseFilter={chooseFilter} chooseCategory={chooseCategory} products={products} removeFilter={removeFilter} chooseColor={chooseColor} chooseSize={chooseSize} size={size} color={color}/>
-        <div className="desktopHeader">
-          <img src={logoLarge} />
-          <div className="helper">
-            <div className="icons">
-              <i className="fas fa-user-circle" onClick={loggedIn ? () => setNav({path: "/profile", direction: 1}) : () => popupLoginFunc()} ></i>
-            </div>
-            <div className="icons">
-              <button className="shopping-cart-btn">
-              <i className="fas fa-shopping-bag" onClick={() => setNav({path: "/cart", direction: 1})}></i>
-                {user && user.cart && user.cart.length > 0 ? (
-                  <div className="cart-count-home">{user.cart.length}</div>
-                ):null}
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className="mobileHeader">
-          <i className="fas fa-user-circle" onClick={() => setNav({path: "/profile", direction: 1})}></i>
-          <h1 className ="profileTitle">bopshop.</h1>
-          <button className="shopping-cart-btn">
-          <i className="fas fa-shopping-bag" onClick={() => setNav({path: "/cart", direction: 1})}></i>
-            {user && user.cart && user.cart.length > 0 ? (
-              <div className="cart-count">{user.cart.length}</div>
-            ):null}
-          </button>
-        </div>
+        {/* Different menus/headers depending on if the user is on deskyop or mobile */}
+        <SideMenu chooseFilter={chooseFilter} chooseCategory={chooseCategory} products={products} removeFilter={removeFilter} 
+          chooseColor={chooseColor} chooseSize={chooseSize} size={size} color={color}/>
+        <DesktopHeader popupLoginFunct={popupLoginFunc} loggedIn={loggedIn} />
+        <MobileHeader />
       </div>
       {/* Buttons that sort via categories or gender */}
-      <div className="sorting-buttons">
-        <div className="dropdown-container">
-          <button type="button" className="button" onClick={toggleDropdown}>KATEGORI <i className="fas fa-chevron-down"></i></button>
-        </div>
-        <div className="line" />
-        <div className="dropdown-container-filter">
-          <button type="button" className="button-filter" onClick={toggleDropdownFilter}>FILTER <i className="fas fa-sliders-h"></i></button>
-        </div>
-      </div>
-      {dropdown && <div className="dropdown">
-        <ul>
-          <li id={99} onClick={chooseCategory} className="bold">all items.</li>
-          <li id={0} onClick={chooseCategory}>Shirts</li>
-          <li id={1} onClick={chooseCategory}>Pants</li>
-          <li id={2} onClick={chooseCategory}>Shoes</li>
-        </ul>
-      </div>}
-      {dropdown && 
-        <div className="selected-container">
-          <div className="selected-width">
-            <div className={`${category == 99 ? "selected-allitems" : category == 0 ? "selected-shirts" : category == 1 ? "selected-pants" : category == 2 ? "selected-shoes" : "unselected"}`}></div>
-          </div>
-        </div>}
-      {dropdownFilter && <div className="dropdown-filter">
-        <ul className="gender-ul">
-          <li id={0} onClick={chooseFilter} className="bold">men.</li>
-          <li id={1} onClick={chooseFilter} className="bold">women.</li>
-        </ul>
-        <div className="divider" />
-        <ul className="filter-ul">
-          {products[0] && products[0].colors.map(Color => (
-            <li id={Color.name} onClick={chooseColor} key={Color.name} style={{background: Color.hex}} className="choose-color">
-              <div className="selected-color" style={color === Color.name ? null : {display: "none"}}>
-                <i className="fas fa-check"></i>
-              </div>
-            </li>
-          ))}
-          {products[0] && products[0].sizes.map(Sizes => (
-            <li id={Sizes} key={Sizes} onClick={chooseSize} className={`choose-size ${Sizes === size ? "selected-size" : ""}`}>{Sizes}</li>
-            // <li key={size} className={`size-btn ${selectedSize === size ? "selected" : ""}`} onClick={() => setSelectedSize(size)}>{size}</li>
-          ))}
-        </ul>
-        <div className="divider" />
-        <ul className="remove-ul">
-          <li onClick={removeFilter} className="bold">clear filters.</li>
-        </ul>
-      </div>}
+      <SortingButtons toggleDropdown={toggleDropdown} toggleDropdownFilter={toggleDropdownFilter} dropdown={dropdown} 
+        chooseCategory={chooseCategory} category={category} dropdownFilter={dropdownFilter} chooseFilter={chooseFilter} products={products}
+        removeFilter={removeFilter} size={size} color={color} chooseColor={chooseColor} chooseSize={chooseSize} />
       {/* Change titel depending on the categorie and gender */}
       <h1 className="browsing">{ gender == 1 ? "women." : "men." } { category == 0 ? "shirts." : category == 1 ? "pants." : category == 2 ? "shoes." : null }</h1>
       {/* The list of all the products matching the given parameters */}
---
+++ client/src/components/HomePage/DesktopHeader.jsx
@@ -0,0 +1,32 @@
+import React, { useContext } from 'react'
+import logoLarge from '../../img/logo-large.svg'
+import { UserContext } from "../../context/UserContext";
+import { NavContext } from "../../context/NavContext";
+
+function DesktopHeader({popupLoginFunc, loggedIn}) {
+    const { user } = useContext(UserContext);
+    const { setNav } = useContext(NavContext);
+
+    return (
+        <>
+            <div className="desktopHeader">
+                <img src={logoLarge} />
+                <div className="helper">
+                    <div className="icons">
+                    <i className="fas fa-user-circle" onClick={loggedIn ? () => setNav({path: "/profile", direction: 1}) : () => popupLoginFunc()} ></i>
+                    </div>
+                    <div className="icons">
+                    <button className="shopping-cart-btn">
+                    <i className="fas fa-shopping-bag" onClick={() => setNav({path: "/cart", direction: 1})}></i>
+                        {user && user.cart && user.cart.length > 0 ? (
+                        <div className="cart-count-home">{user.cart.length}</div>
+                        ):null}
+                    </button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default DesktopHeader
