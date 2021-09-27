2fe9f83743f9c506978d7888ed983bae516a829e
Alexander Gustafsson <alegus753@gmail.com>
2021-09-27 15:28:30 +0200

    mobile fixes

--- client/src/Pages/Wishlist.jsx
+++ client/src/Pages/Wishlist.jsx
@@ -1,38 +1,15 @@
 import React from 'react'
+import MobileHeader from '../components/HomePage/MobileHeader'
 import logo from "../img/logo.svg"
-import { useHistory } from "react-router-dom";
 
 
 function Wishlist() {
   return (
     <div className="page">
-      <div className="wishHeader">
-        <i className="fas fa-user-circle"></i>
-         <img src={logo} />
-        <i class="fas fa-shopping-bag"></i>
+      <DesktopHeader />
+      <div>
+        
 
-      </div>
-       <div className="wishAlbum">
-
-        <p className="albumText">Gillade Artiklar</p>
-        <div>
-          <img className="wishItem" src="./src/Women.png" alt="Woman" ></img>
-          <p className="articleText">Artikel 1</p>
-        </div>
-        <div>
-         <img className="wishItem" src="./src/Men.png" alt="Man"></img>
-         <p className="articleText">Artikel 2</p>
-
-        </div>
-        <div>
-         <img className="wishItem" src="./src/Men.png" alt="Man"></img>
-         <p className="articleText">Artikel 3</p>
-        </div>
-        <div>
-         <img className="wishItem" src="./src/Men.png" alt="Man"></img>
-         <p className="articleText">Artikel 4</p>
-        </div>
-       
       </div>
     </div>
   )
