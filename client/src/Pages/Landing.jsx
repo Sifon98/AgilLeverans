925ea6112ae3c144a13262676ed2c856952469c1
Oliver Carlsson <oliver@framecore.se>
2021-09-30 09:37:17 +0200

    update links

--- client/src/Pages/Landing.jsx
+++ client/src/Pages/Landing.jsx
@@ -15,13 +15,13 @@ function Landing() {
         <h1 className="title">bopshop.</h1>
       </div>
       <div className="downloads-container">
-        <a href="https://edef4.pcloud.com/cBZ8ot9aVZ6iBktJZZZbGLiv7Z2ZZscRZkZY7JeHZVXZbXZ87ZTkZBZvZLXZ5XZEkZn7Z2kZrkZE7ZYXZcXj5ZlpiKP07SpvyWFvziulIad5SunqK7/Bopshop%20Windows%20Installer.zip">
+        <a href="https://edef4.pcloud.com/cBZ8ot9aVZ6iBktJZZZ4Gsiv7Z2ZZscRZkZY7JeHZVXZbXZ87ZTkZBZvZLXZ5XZEkZn7Z2kZrkZE7ZYXZcXj5Z0Ep4XEMPXe7RvTYmBOHXsubecRGV/Bopshop%20Windows%20Installer.zip">
           <button className="download-btn">
             <div className="label"><span>Windows installer</span><i className="fab fa-windows"></i></div>
             <div className="text">Download Bopshop</div>
           </button>
         </a>
-        <a href="https://edef4.pcloud.com/cBZKWGMaVZwHiXtJZZZjb4iv7Z2ZZscRZkZ5zlQ5ZjkZpXZ7XZhZl7ZXXZhXZ3Z1kZOkZ8kZD7ZlkZ2kZr0j5ZgvJvKAGeDn7cvStjxvWPHFbKqkry/Bopshop.zip">
+        <a href="https://edef4.pcloud.com/cBZKWGMaVZwHiXtJZZZVvsiv7Z2ZZscRZkZ5zlQ5ZjkZpXZ7XZhZl7ZXXZhXZ3Z1kZOkZ8kZD7ZlkZ2kZr0j5Z4tlSIlvDelQW3YmYODTzxfNMMAn7/Bopshop.zip">
           <button className="download-btn">
             <div className="label"><span>MacOS</span><i className="fab fa-apple"></i></div>
             <div className="text">Download Bopshop</div>
